b8fe6bcb0906aa2d76e6c36ce55c2d5a69ef5af6
cozy06 <jamesjin06@gmail.com>
2022-10-30 18:06:03 +0900

    촬영 후 face-mesh 그리기

--- capture_landmark.py
+++ capture_landmark.py
@@ -1,11 +1,76 @@
 import cv2
 import mediapipe as mp
 
-# 이미지 읽기
-def face(img) :
+def face_video(num):
+    img_count = 0
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
     mp_face_mesh = mp.solutions.face_mesh
+
+    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+    cap = cv2.VideoCapture(0)
+    with mp_face_mesh.FaceMesh(
+            max_num_faces=1,
+            refine_landmarks=True,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as face_mesh:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("웹캠을 찾을 수 없습니다.")
+                # 비디오 파일의 경우 'continue'를 사용하시고, 웹캠에 경우에는 'break'를 사용하세요
+                continue
+
+            # 필요에 따라 성능 향상을 위해 이미지 작성을 불가능함으로 기본 설정합니다.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = face_mesh.process(image)
+
+            # 이미지 위에 얼굴 그물망 주석을 그립니다.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles
+                        .get_default_face_mesh_tesselation_style())
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_CONTOURS,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles
+                        .get_default_face_mesh_contours_style())
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_IRISES,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles
+                        .get_default_face_mesh_iris_connections_style())
+            # 보기 편하게 이미지를 좌우 반전합니다.
+            cv2.imshow('MediaPipe Face Mesh(Puleugo)', cv2.flip(image, 1))
+            if cv2.waitKey(5)== ord('q'):
+                img_count += 1
+                ret, frame = cap.read()
+                frame = cv2.flip(frame, 1)
+                cv2.imwrite(f'capture/test-{img_count}.jpg', frame)  # 사진 저장
+                if img_count == num :
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    break
+    cap.release()
+
+
+# 이미지 읽기
+def face_img(img, i):
+    # mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    # mp_face_mesh = mp.solutions.face_mesh
 
     # 얼굴 검출을 위한 객체
     mp_face_mesh = mp.solutions.face_mesh
@@ -16,8 +81,8 @@ def face(img) :
     )
     # Face Mesh를 그리기 위한 객체
     mp_drawing = mp.solutions.drawing_utils
-    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    
+    # drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
     annotated_image = cv2.imread(img)
 
     # 얼굴 검출
@@ -46,9 +111,11 @@ def face(img) :
             connection_drawing_spec=mp_drawing_styles
             .get_default_face_mesh_iris_connections_style())
 
-    i = 0
-    result_image_name = "face-mesh" + str(i) + "jpg"
-    cv2.imwrite("face-mesh.jpg", annotated_image)
+    cv2.imwrite(f"result/face-mesh-{i}.jpg", annotated_image)
     return face_landmarks
 
-face("test.png")
+
+face_video(10)
+for i in range(1, 11) :
+    print(i)
+    face_img(f"test.jpg-{i}", i)
